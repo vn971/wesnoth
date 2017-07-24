c4e5774514136eec3401ff5e45b4065ff9afafe8
Charles Dang <exodia339@gmail.com>
2017-07-24 17:45:25 +1100

    Added new minimap drawing method that works wholly with textures
    I've left the old getMinimap function alone and added a new function that will eventually replace it
    that utilizes texture rendering to a given texture instead of relying on surface blits and scaling.
    This has the benefit of, well, reducing surface ops, for one, as well as removing the need to create
    a new texture from the minimap surface every time it's updated.

--- src/minimap.hpp
+++ src/minimap.hpp
@@ -14,18 +14,35 @@
 
 #pragma once
 
+#include "global.hpp"
+
 #include <cstddef>
 #include <map>
 
 class gamemap;
 class surface;
 class team;
+class texture;
 struct map_location;
 class gamemap;
 
+namespace image
+{
+/// function to create the minimap for a given map
+/// the surface returned must be freed by the user
+DEPRECATED("") surface getMinimap(int w,
+		int h,
+		const gamemap& map_,
+		const team* vm = nullptr,
+		const std::map<map_location, unsigned int>* reach_map = nullptr,
+		bool ignore_terrain_disabled = false);
 
-namespace image {
-	///function to create the minimap for a given map
-	///the surface returned must be freed by the user
-	surface getMinimap(int w, int h, const gamemap &map_, const team *vm = nullptr, const std::map<map_location,unsigned int> *reach_map = nullptr, bool ignore_terrain_disabled = false);
+/**
+ * Renders the minimap to the given texture.
+ */
+void render_minimap(texture& tex,
+		const gamemap& map,
+		const team* vw = nullptr,
+		const std::map<map_location, unsigned int>* reach_map = nullptr,
+        bool ignore_terrain_disabled = false);
 }
